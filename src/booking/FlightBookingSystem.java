b0c6caa8a811f2f715e8abeb2b682f92fdeac52a
Louis Rau <122269218+lrau22@users.noreply.github.com>
2024-05-11 17:01:36 +0200

    FlightBookingSystem / HotelBookingSystem

--- src/booking/FlightBookingSystem.java
+++ src/booking/FlightBookingSystem.java
@@ -1,18 +1,103 @@
 package booking;
 
-public class FlightBookingSystem implements BookingSystem{
+import misc.MessageSenderService;
+import misc.PropertyLoader;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashMap;
+import java.util.Random;
+
+public class FlightBookingSystem implements BookingSystem {
+
+    private int port;
+    private int airlineNumber;
+    private final int flightPortStart = Integer.parseInt(PropertyLoader.loadProperties().getProperty("bookingsystems.flight.port.start"));
+    private final int minSeats = Integer.parseInt(PropertyLoader.loadProperties().getProperty("bookingsystems.flight.quantity.min"));
+    private final int maxSeats = Integer.parseInt(PropertyLoader.loadProperties().getProperty("bookingsystems.flight.quantity.max"));
+    private int seats = new Random().nextInt(minSeats, maxSeats);
+    private HashMap<Integer, String> airlineList = new HashMap<>();
+    private final int airlines = flightPortStart + Integer.parseInt(PropertyLoader.loadProperties().getProperty("bookingsystems.flight.amount"));
+    private String[] names = {"Zaun Airways", "Demacia Airways", "Air Piltover", "Fly Freljord", "Air Noxus", "Ionia Air", "Bandle Airways", "Shurima Skyline", "Bilgewater Airways", "Fly Void"};
+
+
+    public FlightBookingSystem(int port) {
+        this.port = port;
+    }
+
+    public void start(int backlog){
+        for(int i = flightPortStart; i < airlines; i++){
+            airlineList.put(i, names[new Random().nextInt(names.length)]);
+        }
+        try(ServerSocket serverSocket = new ServerSocket(port, backlog)) {
+            System.out.println("FlightBookingSystem running on port " + port);
+            while (true) {
+                Socket flightSocket = serverSocket.accept();
+                Thread flightThread = new Thread(() -> {
+                    try {
+                        BufferedReader in = new BufferedReader(new InputStreamReader(flightSocket.getInputStream()));
+                        String inputLine;
+                        while ((inputLine = in.readLine()) != null) {
+                            //Handle message and answer
+                            handleRequest(inputLine, flightSocket);
+                            System.out.println("FlightBookingSystem - Received message: " + inputLine);
+                        }
+                        in.close();
+                        flightSocket.close();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                });
+                flightThread.start();
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public void handleRequest(String booking, Socket flightSocket){
+        //splitMessage -> [0]=BookingRq [1]=UUID [2]=Menge
+        String[] splitMessage = booking.split(" ", 3);
+        String processId = splitMessage[1];
+        String whatAmI = splitMessage[0];
+        airlineNumber = port - flightPortStart;
+        boolean successful;
+        int requestedSeats = Integer.parseInt(splitMessage[2]);
+        if (whatAmI.equals("BookingRq")) {
+            successful = this.book(requestedSeats);
+            //<WhatAmI> <processId> <confirmation (true/false)> <type> <Flightnumber> <amount>
+            MessageSenderService.sendMessageToMessageBroker("Response " + processId + " " + successful+ " flight F" + airlineNumber + " " + requestedSeats);
+        }else if(whatAmI.equals("CancellationRq")) {
+            successful = this.cancel(requestedSeats);
+            //<WhatAmI> <processId> <false>
+            MessageSenderService.sendMessageToMessageBroker("CancellationConfirmation " + processId + " " + successful);
+        }
+    }
+
+
     @Override
-    public boolean cancel(String cancellation) {
-        return false;
+    public boolean cancel(int requestedSeats) {
+        this.seats += requestedSeats;
+        System.out.println("HotelBookingSystem: " + requestedSeats + " rooms could not be booked. Remaining rooms: " + seats + ".");
+        return true;
     }
 
     @Override
-    public boolean book(String booking) {
-        return false;
+    public boolean book(int requestedSeats) {
+        if (requestedSeats <= this.seats) {
+            this.seats -= requestedSeats;
+            System.out.println("HotelBookingSystem: " + requestedSeats + " seats booked. Remaining seats: " + seats + ".");
+            return true;
+        } else {
+            System.out.println("HotelBookingSystem: " + requestedSeats + " seats could not be booked. Remaining seats: " + seats + ".");
+            return false;
+        }
     }
 
     @Override
-    public String getSystemName() {
-        return null;
+    public String getName() {
+        return airlineList.get(this.port);
     }
 }
--- src/booking/HotelBookingSystem.java
+++ src/booking/HotelBookingSystem.java
@@ -1,14 +1,101 @@
 package booking;
 
+import misc.MessageSenderService;
+import misc.PropertyLoader;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashMap;
+import java.util.Random;
+
 public class HotelBookingSystem implements BookingSystem {
+
+    private int port;
+    private int hotelNumber;
+    private final int hotelPortStart = Integer.parseInt(PropertyLoader.loadProperties().getProperty("bookingsystems.hotel.port.start"));
+    private final int minRooms = Integer.parseInt(PropertyLoader.loadProperties().getProperty("bookingsystems.hotel.quantity.min"));
+    private final int maxRooms = Integer.parseInt(PropertyLoader.loadProperties().getProperty("bookingsystems.hotel.quantity.max"));
+    private int rooms = new Random().nextInt(minRooms, maxRooms);
+    private HashMap<Integer, String> hotelList = new HashMap<>();
+    private final int hotels = hotelPortStart + Integer.parseInt(PropertyLoader.loadProperties().getProperty("bookingsystems.hotel.amount"));
+    private String[] names = {"Schachtelwirt", "Hotel zur Kluft", "Gasthof zum Löwen", "Hotel zur Post", "Hotel zur Sonne", "Hotel zum Bären", "Hotel zum Hirschen", "Hotel zum Ochsen", "Hotel zum Schwan", "Hotel zum Stern", "Hotel zum Storchen", "Hotel zum Taunus", "Hotel zum Turm", "Hotel zum weißen Ross", "Hotel zum weißen Schwan", "Hotel zur alten Post", "Hotel zur alten Schule", "Hotel zur alten Stadtmauer"};
+
+
+    public HotelBookingSystem(int port) {
+        this.port = port;
+    }
+
+    public void start(int backlog){
+        for(int i = hotelPortStart; i < hotels; i++){
+            hotelList.put(i, names[new Random().nextInt(names.length)]);
+        }
+        try(ServerSocket serverSocket = new ServerSocket(port, backlog)) {
+            System.out.println("HotelBookingSystem running on port " + port);
+            while (true) {
+                Socket hotelSocket = serverSocket.accept();
+                Thread hotelThread = new Thread(() -> {
+                    try {
+                        BufferedReader in = new BufferedReader(new InputStreamReader(hotelSocket.getInputStream()));
+                        String inputLine;
+                        while ((inputLine = in.readLine()) != null) {
+                            //Handle message and answer
+                            handleRequest(inputLine, hotelSocket);
+                            System.out.println("HotelBookingSystem - Received message: " + inputLine);
+                        }
+                        in.close();
+                        hotelSocket.close();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                });
+                hotelThread.start();
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public void handleRequest(String booking, Socket hotelSocket){
+        //splitMessage -> [0]=BookingRq [1]=UUID [2]=Menge
+        String[] splitMessage = booking.split(" ", 3);
+        String processId = splitMessage[1];
+        String whatAmI = splitMessage[0];
+        hotelNumber = port - hotelPortStart;
+        boolean successful;
+        int requestedRooms = Integer.parseInt(splitMessage[2]);
+        if (whatAmI.equals("BookingRq")) {
+            successful = this.book(requestedRooms);
+            //<WhatAmI> <processId> <confirmation (true/false)> <type> <Hotelnumber> <amount>
+            MessageSenderService.sendMessageToMessageBroker("Response " + processId + " " + successful+ " hotel H" + hotelNumber + " " + requestedRooms);
+        }else if(whatAmI.equals("CancellationRq")) {
+            successful = this.cancel(requestedRooms);
+            //<WhatAmI> <processId> <false>
+            MessageSenderService.sendMessageToMessageBroker("CancellationConfirmation " + processId + " " + successful);
+        }
+
+
+    }
+
+
     @Override
-    public boolean cancel(String cancellation) {
-        return false;
+    public boolean cancel(int requestedRooms) {
+        this.rooms += requestedRooms;
+        System.out.println("HotelBookingSystem: " + requestedRooms + " rooms could not be booked. Remaining rooms: " + rooms + ".");
+        return true;
     }
 
     @Override
-    public boolean book(String booking) {
-        return false;
+    public boolean book(int requestedRooms) {
+        if (requestedRooms <= this.rooms) {
+            this.rooms -= requestedRooms;
+            System.out.println("HotelBookingSystem: " + requestedRooms + " rooms booked. Remaining rooms: " + rooms + ".");
+            return true;
+        }else {
+            System.out.println("HotelBookingSystem: " + requestedRooms + " rooms could not be booked. Remaining rooms: " + rooms + ".");
+            return false;
+        }
     }
 
     @Override
