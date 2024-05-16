6bd188fd7801be8ebea6b3b672e50ba176cbb046
DavidAEW <d4v1d43w@gmail.com>
2024-05-16 08:56:20 +0200

    Service Executor entfernt

--- src/misc/MessageSenderService.java
+++ src/misc/MessageSenderService.java
@@ -40,45 +40,21 @@ public class MessageSenderService {
         if(messageSplit[0].equalsIgnoreCase("CancellationRq")){
             //<WhatAmI> <ProcessId> <Quantity>
             String newMessage = "CancellationRq " + messageSplit[1] + " " + messageSplit[4];
-
-            ExecutorService executor = Executors.newSingleThreadExecutor();
-            Future<String> future = executor.submit(new SendMessageOrTimeout(newMessage, port));
-            try {
-                future.get(60, TimeUnit.SECONDS);
-            } catch (TimeoutException e) {
-                future.cancel(true);
-                sendError(message);
-            } catch (Exception e){
-                e.printStackTrace();
-            }
-            executor.shutdownNow();
+            MessageSenderService.sendMessageToPort(newMessage,port);
             return;
         }
 
         String newMessage = "BookingRq " + messageSplit[1] + " " + messageSplit[4];
-        ExecutorService executor = Executors.newSingleThreadExecutor();
-        Future<String> future = executor.submit(new SendMessageOrTimeout(newMessage, port));
-        try {
-            future.get(60, TimeUnit.SECONDS);
-        } catch (TimeoutException e) {
-            future.cancel(true);
-            sendError(message);
-        } catch (Exception e){
-            e.printStackTrace();
-        }
-        executor.shutdownNow();
+        MessageSenderService.sendMessageToPort(newMessage,port);
     }
 
-    public static boolean sendMessageToPort(String message, int port) {
+    private static void sendMessageToPort(String message, int port) {
         try (Socket socket = new Socket("localhost", port)) {
             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
             out.println(message);
-        } catch (ConnectException e){
-            return false;
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return true;
     }
     public static void sendMessageWithSocket(String message, Socket socket) {
         try {
@@ -101,26 +77,5 @@ public class MessageSenderService {
         } catch (Exception e) {
             e.printStackTrace();
         }
-    }
-}
-
-class SendMessageOrTimeout implements Callable<String> {
-    private String message;
-    private int port;
-
-    public SendMessageOrTimeout(String message, int port){
-        this.message = message;
-        this.port = port;
-    }
-    @Override
-    public String call() throws Exception {
-        try{
-            while(!MessageSenderService.sendMessageToPort(this.message, this.port)){
-                Thread.sleep(5000);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return "message sent successfully!";
     }
 }
